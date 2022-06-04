dacc7d0307884caffc297ab44b284f12e6ce09f1
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-03 23:59:34 -0500

    attach event listeners for nav bar

--- js/app.js
+++ js/app.js
@@ -8,11 +8,20 @@
 
 /*------------------------ Cached Element References ------------------------*/
 
+const aboutBtn = document.querySelector('#about')
+const workBtn = document.querySelector('#work')
+const resumeBtn = document.querySelector('#resume')
+const contactBtn = document.querySelector('#contact')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-
+aboutBtn.addEventListener('click', handleClick)
+workBtn.addEventListener('click', handleClick)
+resumeBtn.addEventListener('click', handleClick)
+contactBtn.addEventListener('click', handleClick)
 
 /*-------------------------------- Functions --------------------------------*/
 
+function handleClick() {
+  console.log('click')
+}
