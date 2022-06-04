3c5bd23cef6262ceb8aa9724963eaaee95c4876d
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-04 15:00:29 -0500

    add copy for resume, add styling for responsive design

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ homeBtn.addEventListener("click", homePage)
 /*-------------------------------- Functions --------------------------------*/
 function homePage() {
   pageText.innerHTML =
-    "Nick is a Houston, TX based <br />software engineer and musician.";
+    "Nick is a Houston, TX based software engineer and musician.";
   titleText.textContent = "Nicholas Bailey";
   imageSpace.innerHTML =
     '<img src ="./assets/profilepic.jpg" style="width: auto;height: auto;max-width: 350px;max-height:350px;">';
@@ -32,11 +32,12 @@ function homePage() {
 function showContact() {
   pageText.innerHTML = "Here's where to get in contact with me:";
   imageSpace.innerHTML =
-    '<img src="./assets/github.jpg" style ="width: auto;height: auto;max-width: 200px;max-height:200px;"><img src="./assets/linkedin.jpeg" style="width: auto;height: auto;max-width: 200px;max-height:200px;><img src="./assets/gmail.jpeg" style="width: auto;height: auto;max-width: 200px;max-height:200px;">';
+    '<img src="./assets/GitHub.png" style ="width: auto;height: auto; padding-right: 10%;"><img src="./assets/linkedin.png" style="width: auto;height: auto;max-width: 128px;max-height:128px;><img src="./assets/gmail.jpeg" style="width: auto;height: auto;max-width: 200px;max-height:200px;">';
   titleText.textContent = "Contact";
 }
 function showResume() {
-  pageText.innerHTML = "My previous experience";
+  pageText.innerHTML = "";
+  imageSpace.innerHTML = "<p style ='text-align: left;grid-area: text-area;font-family: var(--sans-serif);font-size: 2em;font-weight: 400;padding-right: 5%;padding-top: 10%;padding-left: 1ch; max-width: 65ch;'<br />Currently: <br /> Attending General Assembly software engineering immersive bootcamp. Learning full-stack software development and design.<br /><br /> 2019-2022: <br />Medical Records Bookmarker at Veterans Evaluation Services. Assisted medical practitioners by providing condensed files of relevant medical information to expedite veteran disability claims. <br /><br />2017-2019: <br /> Banking industry. Worked as a teller and member service representative at a bank and a credit union.<br /></p>"
   titleText.textContent = "Resume";
 }
 function showAbout() {
