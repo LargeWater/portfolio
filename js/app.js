1b5e6f84ee259d60347fd40d78cf5d3270f7908b
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-06 16:12:13 -0500

    switch work data to data.js

--- js/app.js
+++ js/app.js
@@ -1,73 +1,76 @@
 /*-------------------------------- Constants --------------------------------*/
-import { aboutMeText, resumeHtml, contactHtml, homeHtml, works } from "./data.js"
+import {
+  aboutMeText,
+  resumeHtml,
+  contactHtml,
+  homeHtml,
+  works,
+} from "./data.js";
 /*-------------------------------- Variables --------------------------------*/
 /*------------------------ Cached Element References ------------------------*/
-const aboutBtn = document.querySelector("#about")
-const workBtn = document.querySelector("#work")
-const resumeBtn = document.querySelector("#resume")
-const contactBtn = document.querySelector("#contact")
-const titleText = document.querySelector("#title")
-const pageText = document.querySelector("#text-area")
-const imageSpace = document.querySelector("#image-area")
-const homeBtn = document.querySelector("#home-btn")
+const aboutBtn = document.querySelector("#about");
+const workBtn = document.querySelector("#work");
+const resumeBtn = document.querySelector("#resume");
+const contactBtn = document.querySelector("#contact");
+const titleText = document.querySelector("#title");
+const pageText = document.querySelector("#text-area");
+const imageSpace = document.querySelector("#image-area");
+const homeBtn = document.querySelector("#home-btn");
 /*----------------------------- Event Listeners -----------------------------*/
-aboutBtn.addEventListener("click", showAbout)
-workBtn.addEventListener("click", showWork)
-resumeBtn.addEventListener("click", showResume)
-contactBtn.addEventListener("click", showContact)
-homeBtn.addEventListener("click", homePage)
+aboutBtn.addEventListener("click", showAbout);
+workBtn.addEventListener("click", showWork);
+resumeBtn.addEventListener("click", showResume);
+contactBtn.addEventListener("click", showContact);
+homeBtn.addEventListener("click", homePage);
 /*-------------------------------- Functions --------------------------------*/
 function homePage() {
-  resetFont()
+  resetFont();
   pageText.innerHTML =
-    "Nick is a Houston, TX based software engineer and musician."
-  titleText.textContent = "Nicholas Bailey"
-  imageSpace.innerHTML = homeHtml
+    "Nick is a Houston, TX based software engineer and musician.";
+  titleText.textContent = "Nicholas Bailey";
+  imageSpace.innerHTML = homeHtml;
 }
 function showContact() {
-  resetFont()
-  contactBtn.style.fontSize = "4em"
-  pageText.innerHTML = "Here's where to get in contact with me:"
-  imageSpace.innerHTML = contactHtml
-  titleText.textContent = ""
+  resetFont();
+  contactBtn.style.fontSize = "4em";
+  pageText.innerHTML = "Here's where to get in contact with me:";
+  imageSpace.innerHTML = contactHtml;
+  titleText.textContent = "";
 }
 function showResume() {
   resetFont();
-  resumeBtn.style.fontSize = "4em"
-  pageText.innerHTML = resumeHtml
-  imageSpace.innerHTML = ''
-  titleText.textContent = ""
+  resumeBtn.style.fontSize = "4em";
+  pageText.innerHTML = resumeHtml;
+  imageSpace.innerHTML = "";
+  titleText.textContent = "";
 }
 function showAbout() {
   resetFont();
-  aboutBtn.style.fontSize = "4em"
-  pageText.innerHTML = aboutMeText
-  imageSpace.innerHTML = ""
-  titleText.textContent = ""
+  aboutBtn.style.fontSize = "4em";
+  pageText.innerHTML = aboutMeText;
+  imageSpace.innerHTML = "";
+  titleText.textContent = "";
 }
+
 function showWork() {
   resetFont();
-  workBtn.style.fontSize = "4em"
-  pageText.innerHTML = "Here are some projects I've done:"
-  // imageSpace.innerHTML = workHtml
-  titleText.textContent = ""
-  imageSpace.innerHTML = works.map(work => {
-    `<section>
-    <div>${work.title}</div>
-    <div>${work.description}</div>
-    <div> <a href ="${work.github}" target="_blank">GitHub</a></div>
-    <div><a href="${work.deploy}" target="_blank"><img src="${work.image}"></div>
-    </section>`
-  })
-  console.log(imageSpace.innerHTML)
+  workBtn.style.fontSize = "4em";
+  imageSpace.innerHTML = "";
+  titleText.textContent = "";
+  works.map(({ title, image, description, github, deploy }) => {
+    pageText.innerHTML = `<div>
+      <div>${title}</div>
+      <div>${description}</div>
+      <div> <a href ="${github}" target="_blank">GitHub</a></div>
+      <div><a href="${deploy}" target="_blank"><img src="${image}"></div>
+    </div>`;
+  });
 }
+
 function resetFont() {
-  aboutBtn.style.fontSize = "2em"
-  homeBtn.style.fontSize = "2em"
-  contactBtn.style.fontSize = "2em"
-  workBtn.style.fontSize = "2em"
-  resumeBtn.style.fontSize = "2em"
+  aboutBtn.style.fontSize = "2em";
+  homeBtn.style.fontSize = "2em";
+  contactBtn.style.fontSize = "2em";
+  workBtn.style.fontSize = "2em";
+  resumeBtn.style.fontSize = "2em";
 }
-// function clearPage() {
-//   const 
-// }
